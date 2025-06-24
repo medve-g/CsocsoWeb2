3a0a3b55b8ae67071746f60c03756b4aa4a75ba1
medve.gergo <medve.gergo@gallicoop.hu>
2025-06-24 10:37:51 +0200

    Welcome page first section done and responsive

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { WelcomePage } from './pages/welcome-page/welcome-page';
+import { LoginPage } from './pages/login-page/login-page';
 
-export const routes: Routes = [{
-    path: "",
-    component: WelcomePage
-}];
+export const routes: Routes = [
+    {path: "", component: WelcomePage},
+    {path: "login", component: LoginPage}
+];
--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  scrolledDown = false;
+  displayTitle = true
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const yOffset = window.scrollY || window.pageYOffset;
+    this.scrolledDown = yOffset > 80; 
+  }
 }
